ece8418afbbc40ec666b35c70253ebf31b0685b5
Laiba Khan <Laaaibah@gmail.com>
2022-03-02 15:59:43 +0200

    The_Duel 50% code

--- The_Duel/The_Duel/The_Duel.cs
+++ The_Duel/The_Duel/The_Duel.cs
@@ -4,15 +4,59 @@ using Jypeli.Controls;
 using Jypeli.Widgets;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class The_Duel : PhysicsGame
 {
+
+    private string[] lines;
+    private Label[] questions = new Label[4];
+    private Label question = new Label();
+
     public override void Begin()
     {
-        // Kirjoita ohjelmakoodisi tähän
+        string questions = "../../../../../questions.txt";
+        lines = File.ReadAllLines(questions);
+
+        CreateBoxes();
+        
+        int x = RandomGen.NextInt(lines.Length);
+        GenerateQuestionAndAnswers(x);
+
+        
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    private void GenerateQuestionAndAnswers(int lineNumber)
+    {
+
+        string[] data = lines[lineNumber].Split('|');
+        // set the question text 
+        for (int i = 0; i < questions.Length; i++)
+        {
+           Label box = questions[i];
+           box.Text = data[i + 1];
+            
+        }
+    }
+
+    public void CreateBoxes()
+    {
+        // create a question label
+        for (int i = 0; i < answers.Length; i++)
+        {
+           
+            
+          Label box = new Label();
+          box.Y = -20 * i;
+          Add(box);
+          box.Text = "test";
+          questions[i] = box;
+            
+        }
+        
     }
 }
 
